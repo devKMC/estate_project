37c0529c56a270cfff44e898bd4627a77c71b343
devKMC <allcasa@naver.com>
2024-04-24 09:50:43 +0900

    2024-04-24 9:50

---
+++ back/src/main/java/com/estate/back/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.estate.back.config;
+
+//  Spring Web Security 설정
+// - Basic 인증 미사용 으로 변경
+// - CSRF 정책 미사용
+// - Session 생성 정책 미사용
+// - CORS 정책 (모든 출처 - 모든 메서드 - 모든 패턴 허용)
+public class WebSecurityConfig {
+
+}
