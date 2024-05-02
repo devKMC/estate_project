c2d2431dc6c6073736f7b48d26c05b3453a49010
devKMC <allcasa@naver.com>
2024-05-02 12:52:05 +0900

    2024-05-02 12:50

--- front/src/views/service/qna/QnaWrite/index.tsx
+++ front/src/views/service/qna/QnaWrite/index.tsx
@@ -1,22 +1,68 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './style.css'
+import { useUserStore } from 'src/stores';
+import { useNavigate } from 'react-router';
+import { QNA_LIST_ABSOLUTE_PATH } from 'src/constant';
 
 
 
-export default function QnaWrite() {
+export default function QnAWrite() {
 
-     //                    render                    //
+    //                              state                              //
+    const contentsRef = useRef<HTMLTextAreaElement | null>(null);
+    const { loginUserRole } = useUserStore();
+    const [title, setTitle] = useState<string>('');
+    const [contents, setContents] = useState<string>('');
+
+
+    //                              function                              //
+    const navigator = useNavigate();
+
+
+    //                              event Handler                              //
+    const onTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const title = event.target.value;
+        setTitle(title);
+    };
+
+    const onContentsChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const contents = event.target.value;
+        if (contents.length > 1000) return;
+        setContents(contents);
+        if (!contentsRef.current) return;
+        contentsRef.current.style.height = 'auto';
+        contentsRef.current.style.height = `${contentsRef.current.scrollHeight}px`;
+    }
+
+    const onPostButtonClickHandler = () => {
+        if (!title || !contents) return; // 비어있으면 return
+        alert('작성!')
+
+    };
+
+    //                              effect                              //
+    // 화면이 전환될때 작성
+    useEffect(() => {
+
+        if (loginUserRole === 'ROLE_ADMIN') {
+            navigator(QNA_LIST_ABSOLUTE_PATH);
+            return;
+        }
+
+    }, [])
+
+    //                              render                              //
     return (
-        <div id = "qna-write-wrapper">
-            <div className= "qna-write-top">
+        <div id="qna-write-wrapper">
+            <div className='qna-write-top'>
                 <div className='qna-write-title-box'>
-                    <input/>
+                    <input className='qna-write-title-input' placeholder='제목을 입력해 주세요' value={title} onChange={onTitleChangeHandler} />
                 </div>
-                <div>올리기</div>
+                <div className='primary-button' onClick={onPostButtonClickHandler}>올리기</div>
             </div>
-            <div>
-                {/*  두줄이상 작성할때 textarea 사용 */}
-                <textarea /> 
+            <div className='qna-write-contents-box'>
+                {/*  두줄이상 작성할때 textarea 사용,  row로 기본값 10줄짜리 작성, 1000자 제한 가능 */}
+                <textarea ref={contentsRef} className='qna-write-contents-textarea' rows={10} placeholder='내용을 입력해주세요/ 1000자' maxLength={1000} value={contents} onChange={onContentsChangeHandler} />
             </div>
         </div>
     );
