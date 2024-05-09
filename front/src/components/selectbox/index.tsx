8e8506c4a7c2f01c0dcd780489e0e9e59f44e937
devKMC <allcasa@naver.com>
2024-05-09 10:51:50 +0900

    2024-05-09 10:50

---
+++ front/src/components/selectbox/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+
+//                component                //
+export default function SelectBox() {
+    return (
+        <div className='select-box'>
+                            <div className='select-selected'></div>
+                            <div className='select-open-button'></div>
+                        </div>
+    )
+}
